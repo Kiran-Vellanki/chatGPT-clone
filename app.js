dc38bdf5d04d1b6233c4244088090856f9c07643
Kiran Vellanki <114850016+Kiran-Vellanki@users.noreply.github.com>
2023-04-30 13:05:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const API_KEY = "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";
+const API_KEY = "api key";
 const submitBtn=document.querySelector('#submit')
 const output= document.querySelector('#output')
 const historyElement=document.querySelector('.history')
 const buttonElement=document.querySelector('button')
-const names=new Array ("kiran vellanki","kiran","kiran v","vellanki","vellanki kiran");
+
 const inputElement=document.querySelector('input')
 
 function changeInput(value){
@@ -25,15 +25,13 @@ async function getResult() {
         })
     }
  try {
- const response=await fetch('https://api.openai.com/v1/chat/completions', options)
+ const response=await fetch('open ai completions link', options)
     // const response=await.fetch("https://api.openai.com/v1/chat/completions");
    const data=await response.json()
    console.log(data)
-  if(names.includes(String(inputElement.value.toLowerCase()))){
-    output.textContent="he is the developer for this project!\n for more information go to this link  https://portfolio2-591b9.web.app/";
-  }else{
+  
    output.textContent=data.choices[0].message.content
-  }
+  
    if(data.choices[0].message.content){
     const pElement=document.createElement('p')
     pElement.addEventListener('click',()=>changeInput(pElement.textContent))
